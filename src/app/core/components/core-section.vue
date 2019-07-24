8b66857e0578a05c99bb42d9b1ea783051647aab
Antohan <justanton19@gmail.com>
2019-07-24 20:00:03 +0700

    Create core-section component

---
+++ src/app/core/components/core-section.vue
@@ -0,0 +1,60 @@
+<template>
+  <section :class="$style.host">
+    <router-link
+      :to="{ name: routeName }"
+      :class="$style.link">
+      <div :class="$style.subtitle">
+        <span>{{ title }}</span>
+      </div>
+      <div :class="$style.description">
+        {{ description }}
+      </div>
+    </router-link>
+    <div :class="$style.row">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    routeName: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+};
+</script>
+
+<style lang="scss" module>
+@import "@/assets/scss/app.scss";
+
+.host {
+  margin-bottom: 80px;
+}
+
+.row {
+  max-height: 350px;
+  overflow: hidden;
+}
+
+.link {
+  display: block;
+  padding: 0 5px;
+
+  &:hover > .subtitle {
+    color: #ffe81f;
+  }
+}
+
+.subtitle {
+  font-size: 20px;
+  font-weight: bold;
+  line-height: 25px;
+  color: #f4f4f4;
+}
+
+.description {
+  color: $default-color;
+}
+</style>
--- src/plugins/base-components.js
+++ src/plugins/base-components.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 
 import FormGroup from '@/app/core/components/form-group';
 import CoreInput from '@/app/core/components/core-input';
+import CoreSection from '@/app/core/components/core-section';
 
 Vue.component('FormGroup', FormGroup);
 Vue.component('CoreInput', CoreInput);
+Vue.component('CoreSection', CoreSection);

6b8430ff353afa970fbf4c3d71db0db8334dba2d
Antohan <justanton19@gmail.com>
2019-07-25 17:26:33 +0700

    Creating film-info component

---
+++ src/app/films/components/film-info.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="filmInfo">
+    {{ film }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FilmInfo',
+  props: {
+    film: { type: Object, required: true },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/app/films/components/index.js
+++ src/app/films/components/index.js
@@ -1,2 +1,2 @@
-// eslint-disable-next-line import/prefer-default-export
 export { default as FilmsSection } from './films-section';
+export { default as FilmInfo } from './film-info';

982164698fff9f6934f5ddb3482b1c1369552f26
Antohan <justanton19@gmail.com>
2019-08-22 17:56:07 +0700

    Add fetch films api

--- src/app/films/services/index.js
+++ src/app/films/services/index.js
@@ -1,1 +1,23 @@
-export default {};
+import axios from 'axios';
+
+const FILMS_URL = 'https://sw-wiki.firebaseio.com/swapi/films.json';
+
+// eslint-disable-next-line import/prefer-default-export
+export async function getFilmList() {
+  try {
+    const { data } = await axios.get(FILMS_URL);
+
+    const mapData = data.map((film, index) => ({
+      id: index + 1,
+      title: film.fields.title,
+      releaseDate: film.fields.release_date,
+      director: film.fields.director,
+      episodeId: film.fields.episode_id,
+      description: film.fields.opening_crawl,
+    }));
+
+    return mapData;
+  } catch (error) {
+    throw error.error;
+  }
+}
--- src/app/films/store/index.js
+++ src/app/films/store/index.js
@@ -1,20 +1,38 @@
-export const SIGN_IN = 'SIGN_IN';
+import { getFilmList } from '../services';
 
 export const SET_SHOWING_FILM = 'SET_SHOWING_FILM';
+export const FETCH_FILM_LIST = 'FETCH_FILM_LIST';
 
 const state = {
   showFilmInfoId: null,
+  films: [],
 };
 
 const mutations = {
   [SET_SHOWING_FILM]: (state, filmId) => {
     state.showFilmInfoId = filmId;
   },
+
+  [FETCH_FILM_LIST]: (state, filmList) => {
+    state.films = filmList;
+  },
 };
 
 const actions = {
   [SET_SHOWING_FILM]: ({ commit }, filmId) => {
     commit(SET_SHOWING_FILM, filmId);
+  },
+
+  [FETCH_FILM_LIST]: async ({ commit }) => {
+    const filmList = await getFilmList();
+
+    commit(FETCH_FILM_LIST, filmList);
+  },
+};
+
+const getters = {
+  isFilmListEmpty(state) {
+    return state.films.length === 0;
   },
 };
 
@@ -23,4 +41,5 @@ export default {
   state,
   mutations,
   actions,
+  getters,
 };
